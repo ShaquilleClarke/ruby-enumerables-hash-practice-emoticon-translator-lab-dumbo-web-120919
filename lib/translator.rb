c7b515234257b10ba3accd78565b55e9975d5be8
Shaquille Clarke <shaqstaysfresh@gmail.com>
2019-12-06 19:57:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,8 +14,9 @@ def load_library(file_path)
   put_hash # code goes here
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  result = load_library(file_path)['get_emoticon'][emoticon]
+  result ? result : "Sorry, that emoticon was not found"# code goes here
 end
 
 def get_english_meaning
